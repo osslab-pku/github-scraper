1fbf589b997481f7a820a614f8ea04b78e7b108e
hrz6976 <hrz6976@hotmail.com>
2025-09-22 15:49:11 +0800

    feat: parse owner

--- src/pypi/maintainer.ts
+++ src/pypi/maintainer.ts
@@ -4,21 +4,31 @@ import { HTMLParser, fieldMap } from '../common/htmlparser'
 import { OpenAPIRoute, Str, Num, Arr } from 'chanfana'
 
 const MaintainerRequestSchema = z.object({
-    package: Arr(Str({ example: 'requests' }), {
+    package: Arr(Str({ example: 'numpy' }), {
     }).max(100).min(1),
 })
 
 const maintainerResponseSchema = z.object({
-    data: z.record(z.string(), z.string()),
+    data: z.record(z.string(), z.object({
+        owner: z.string(),
+        maintainers: z.array(z.string()),
+    })),
     errors: z.record(z.string(), z.string()),
 })
 
-async function parseMaintainer(response: Response): Promise<string[]> {
+async function parseMaintainer(response: Response): Promise<{ owner: string; maintainers: string[] }> {
     const parser = new HTMLParser()
+    parser.addTextParser('owner', "a.vertical-tabs__tab[href^='/org/']")
     parser.addTextParser('maintainer', 'span.sidebar-section__user-gravatar-text')
     let res = await parser.parse(response)
+    res = fieldMap(res, 'owner', (v) => v.map((t) => t.trim()))
     res = fieldMap(res, 'maintainer', (v) => v.map((t) => t.trim()))
-    return [...new Set(res['$keyIsNull']['maintainer'])]
+    const owner = res['$keyIsNull']['owner']? res['$keyIsNull']['owner'].filter((v) => v.length > 0)[0] : undefined
+    const maintainers = [...new Set(res['$keyIsNull']['maintainer'])]
+    return {
+        owner,
+        maintainers,
+    }
 }
 
 export class GetPypiMaintainer extends OpenAPIRoute {
@@ -41,7 +51,7 @@ export class GetPypiMaintainer extends OpenAPIRoute {
     async handle(request: Request, env: Env, ctx: ExecutionContext) {
         const params = (await this.getValidatedData<typeof this.schema>()).query
         const pkgs = params.package
-        const results: Record<string, string[]> = {}
+        const results: Record<string, { owner: string; maintainers: string[] }> = {}
         const errors: Record<string, string> = {}
         for (const pkg of pkgs) {
             const url = `https://pypi.org/project/${pkg}/`
@@ -49,8 +59,7 @@ export class GetPypiMaintainer extends OpenAPIRoute {
                 console.log(url)
                 const response = await fetchURL(url)
                 if (response.status === 200) {
-                    const maintainers = await parseMaintainer(response)
-                    results[pkg] = maintainers
+                    results[pkg] = await parseMaintainer(response)
                 } else if (response.status === 404) {
                     errors[pkg] = 'package not found'
                 } else {
